11b3527190af9bb5c954a3f83f09c1c6ec015639
belbrun <bruno.belic@fer.hr>
2020-01-08 15:43:56 +0100

    Add neural_net module

---
+++ neural_net.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+def load_data(index):
+    
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.summary()
