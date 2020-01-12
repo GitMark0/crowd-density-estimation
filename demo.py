41f04132a2d39d2110c73235a987d8b9de7f0a81
markovac <tm50053@fer.hr>
2020-01-12 21:17:32 +0100

    Revisited network architecture, added demo images and added simple postprocessing

--- demo.py
+++ demo.py
@@ -1,15 +1,15 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers
 import data_util
 import neural_net
 
 root = 'ShanghaiTech_Crowd_Counting_Dataset'
 folder = 'processed'
 
-def main():
 
+def main():
     input_shape = (128, 170, 3)
     output_shape = (128, 170, 1)
 
@@ -18,27 +18,19 @@ def main():
     model = neural_net.init_model(input_layer, output_shape)
     model.load_weights('weights/')
     folder = 'processed'
-    part_B_train = os.path.join(root, 'part_B_final', 'train_data', folder)
     part_B_test = os.path.join(root, 'part_B_final', 'test_data', folder)
 
-    X_train = data_util.load_data_without_labels(part_B_train, 400)
-    y_train = data_util.load_data_without_labels(part_B_train.replace('processed', 'processed_labels'), 400)
     X_test = data_util.load_data_without_labels(part_B_test, 316)
-    y_test = data_util.load_data_without_labels(part_B_test.replace('processed', 'processed_labels'), 316)
 
-    model.compile(optimizer='adadelta',
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0012),
                   loss=tf.keras.losses.MeanSquaredError(),
                   metrics=['accuracy'],
                   sample_weight_mode='temporal')
 
-    #loss,acc = model.evaluate(X_test,  y_test[0:316], verbose=2)
-    #print("Restored model, accuracy: {:5.2f}%".format(100*acc))
-
-    test_sample = X_test[0:10]
+    test_sample = X_test[0:316]
     prediction = model.predict(test_sample)
-    #print(prediction[1])
-    image = data_util.image_from_arr(prediction[5], gray = False, norm = True)
-    image.show()
+    data_util.show_prediction_as_image(prediction[315])
+
 
 if __name__ == '__main__':
     main()
--- neural_net.py
+++ neural_net.py
@@ -1,9 +1,8 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-from tensorflow.keras import  layers, models
+from tensorflow.keras import layers, models
 import data_util
-from tensorflow import expand_dims
 
 root = 'ShanghaiTech_Crowd_Counting_Dataset'
 
@@ -13,27 +12,28 @@ def init_model(input_layer, output_shape):
     model.add(input_layer)
 
     model.add(layers.BatchNormalization())
-    model.add(layers.Conv2D(2, (3, 3)))
+    model.add(layers.Conv2D(3, (3, 3)))
     model.add(layers.LeakyReLU())
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(6, (2, 2)))
-    model.add(layers.ReLU())
+    model.add(layers.LeakyReLU())
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(10, (3, 3)))
     model.add(layers.ReLU())
 
     model.add(layers.Conv2DTranspose(10, (3, 3)))
     model.add(layers.UpSampling2D())
-    model.add(layers.Dropout(0.5))
+    model.add(layers.Dropout(0.45))
     model.add(layers.ReLU())
     model.add(layers.Conv2DTranspose(6, (2, 2)))
     model.add(layers.UpSampling2D())
-    model.add(layers.Dropout(0.5))
+    model.add(layers.Dropout(0.45))
     model.add(layers.ReLU())
     model.add(layers.Conv2DTranspose(3, (3, 3)))
     model.add(layers.Conv2DTranspose(3, (1, 3)))
 
     return model
+
 
 def main():
     folder = 'processed'
@@ -41,29 +41,26 @@ def main():
     part_B_test = os.path.join(root, 'part_B_final', 'test_data', folder)
 
     X_train = data_util.load_data_without_labels(part_B_train, 400)
-    y_train = data_util.load_data_without_labels(part_B_train.replace('processed','processed_labels'), 400)
+    y_train = data_util.load_data_without_labels(part_B_train.replace('processed', 'processed_labels'), 400)
     X_test = data_util.load_data_without_labels(part_B_test, 316)
-    y_test = data_util.load_data_without_labels(part_B_test.replace('processed','processed_labels'), 316)
+    y_test = data_util.load_data_without_labels(part_B_test.replace('processed', 'processed_labels'), 316)
 
-    input_shape  = (128, 170, 3)
+    input_shape = (128, 170, 3)
     output_shape = (128, 170, 1)
 
-    test = expand_dims(X_train[0], 0)
     input_layer = layers.Input(input_shape)
 
     model = init_model(input_layer, output_shape)
     model.summary()
 
-    model.compile(optimizer=tf.keras.optimizers.Nadam(learning_rate=0.0019),
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0012),
                   loss=tf.keras.losses.MeanSquaredError(),
-                  metrics=['accuracy'],
+                  metrics=['mse'],
                   sample_weight_mode='temporal')
 
-    history = model.fit(X_train, y_train[0:400], epochs=50, validation_data=(X_test, y_test[0:316]), verbose=1)
+    model.fit(X_train, y_train[0:400], epochs=30, validation_data=(X_test, y_test[0:316]), verbose=1)
 
     model.save_weights('weights/')
-
-
 
 
 if __name__ == '__main__':
