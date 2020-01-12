48346b64b2005827fab75ecb481ee839c8cc8247
belbrun <bruno.belic@fer.hr>
2020-01-12 20:06:52 +0100

    Adjust demo to use dataset unrelated images

--- demo.py
+++ demo.py
@@ -4,23 +4,18 @@ import data_util
 import os
 import neural_net
 from keras_preprocessing.image import ImageDataGenerator
+import numpy as np
 
-
-root = 'beijing'
-folder = 'processed'
+root = 'test_examples'
+examples = ['82063735_2863425327047082_1096112625017683968_n.jpg',
+    '82802453_642298606511069_9138862684085682176_n.jpg',
+    '82428967_753940481794855_6610497251762503680_n.jpg']
 
 def main():
 
     input_size = (64, 128, 3)
-    batch_size = 4
-    image_gen = ImageDataGenerator(rescale=1 / 255,
-                               fill_mode='nearest')
 
-    test_image_gen = image_gen.flow_from_directory(
-        os.path.join(root, 'test'),
-        target_size=input_size[:2],
-        batch_size=batch_size,
-        class_mode='categorical')
+    _, X = data_util.load_example(os.path.join(root, examples[2]), input_size)
 
     model = neural_net.init_model()
     model.load_weights('weights/')
@@ -28,10 +23,8 @@ def main():
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
+    print('Prediction: ', model.predict(np.expand_dims(X, axis=0)))
 
-
-    loss,acc = model.evaluate_generator(generator=test_image_gen)
-    print("Restored model, accuracy: {:5.2f}%".format(100*acc))
 
 
 if __name__ == '__main__':
